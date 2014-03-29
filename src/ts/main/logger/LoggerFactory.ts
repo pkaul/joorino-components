0148ab8630bc75063ffe8be56e11844f01056f7d
pkaul <peter@girino.de>
2014-03-29 23:38:12 +0100

    Writing warn message if logger is not initialized

--- src/ts/main/logger/LoggerFactory.ts
+++ src/ts/main/logger/LoggerFactory.ts
@@ -1,17 +1,52 @@
 import Logger = require("./Logger");
+import Console = require("../Console");
+import Strings = require("../Strings");
 
 /**
  * A logger that does not nothing
  */
-class NoopLogger implements Logger {
+//class NoopLogger implements Logger {
+//    public debug(message:string, ...params):void {}
+//    public info(message:string, ...params):void {}
+//    public warn(message:string, ...params):void {}
+//    public error(message:string, ...params):void {}
+//    public isDebugEnabled():boolean { return false; }
+//    public isInfoEnabled():boolean { return false; }
+//    public isWarnEnabled():boolean { return false; }
+//    public isErrorEnabled():boolean { return false; }
+//}
+
+/**
+ * A static logger that writes warn and error messages only
+ */
+class WarnErrorLogger implements Logger {
+
+    private _notConfiguredMessageLogged:boolean = false;
+
     public debug(message:string, ...params):void {}
     public info(message:string, ...params):void {}
-    public warn(message:string, ...params):void {}
-    public error(message:string, ...params):void {}
+    public warn(message:string, ...params):void {
+        this.logNotConfiguredMessage();
+        //Console.warn(Strings.format(message, params));
+    }
+    public error(message:string, ...params):void {
+        this.logNotConfiguredMessage();
+        Console.error(Strings.format(message, params));
+    }
     public isDebugEnabled():boolean { return false; }
     public isInfoEnabled():boolean { return false; }
-    public isWarnEnabled():boolean { return false; }
-    public isErrorEnabled():boolean { return false; }
+    public isWarnEnabled():boolean { return true; }
+    public isErrorEnabled():boolean { return true; }
+
+    /**
+     * Writes a warn message in case that logging is not configured.
+     */
+    private logNotConfiguredMessage():void {
+        if( !this._notConfiguredMessageLogged ) {
+            this._notConfiguredMessageLogged = true;
+            Console.warn("Logging environment has not been configured properly. Level 'error' is enabled only.");
+        }
+    }
 }
 
 /**
@@ -20,7 +55,7 @@ class NoopLogger implements Logger {
 class LoggerFactory {
 
     // a default logger instance
-    private static DEFAULT:Logger = new NoopLogger();
+    private static DEFAULT:Logger = new WarnErrorLogger();
 
     private static delegate:Function = null;
 
